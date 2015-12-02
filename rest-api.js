b04504b98720b3ca9e2806a3295c3c4408e37e1a
Lars Jenzer <lars.jenzer@ch.ibm.com>
2015-12-02 13:31:30 +0000

    line break

--- rest-api.js
+++ rest-api.js
@@ -23,7 +23,7 @@ exports.isActive = function(req, res){
 exports.getCurrentStatus = function(req,res){
 //	res.send('Program selected: Regenerate\r\nTemperature: 110° Celsius');
 //	res.send('temperature: 40C\nprogram chosen');
-	res.send('temperature: 40C' + 'program chosen');
+	res.send('temperature: 40C \ program chosen');
 }
 
 exports.getDeviceName = function(req,res){
