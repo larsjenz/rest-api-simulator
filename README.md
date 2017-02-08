3b2d0bc81b4a6bfad03c342e5d423e66f5fb9af7
Lars Jenzer <lars.jenzer@ch.ibm.com>
2017-02-08 10:24:33 +0000

    changed title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# V-ZUG Simulator
+# Yiannis Simulator
 
 This application is a simple simulator of the V-ZUG API used in appliances and devices.  It is intended to be used 
 by developers writing applications that require V-ZUG API access to devices.
--- rest-api.js
+++ rest-api.js
@@ -1,31 +1,31 @@
 // API handlers for V-ZUG interface
 
 exports.getModel = function( req, res ) {
-	res.send('MSLQ');
+	res.send("MSLQ");
 }
 
 exports.getModelDescription = function(req, res) {
-	res.send('Combi-Steam MSLQ');
+	res.send("Combi-Steam MSLQ");
 }
 
 exports.getMachineType = function(req,res){
-	res.send('ST');
+	res.send("ST");
 }
 
 exports.getSerialNumber = function(req,res){
-	res.send('811858972432');
+	res.send("811858972432");
 }
 
 exports.isActive = function(req, res){
-	res.send ('true');
+	res.send ("true");
 }
 
 exports.getCurrentStatus = function(req,res){
-	res.send('Program selected: Regenerate - Temperature: 110° Celsius');
+	res.send("Program selected: Regenerate - Temperature: 110° Celsius");
 }
 
 exports.getDeviceName = function(req,res){
-	res.send('Stanley Steamer');
+	res.send("Stanley Steamer");
 }
 
 exports.setDeviceName = function(req,res){
